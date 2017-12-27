7f778a902e8344181fe41c7f105f37abe8888361
lijing <lijing@weyao.com>
2017-12-27 16:13:38 +0800

    spring cloud config

--- README.md
+++ README.md
@@ -4,16 +4,18 @@
 任务进行时会更新status字段，修改配置时需先根据status判断任务是否正在运行。
 可以做一个web服务提供增删改查功能。
 
+注：该项目不是web项目，不能用bus刷新配置；即使配置成了web项目，schedule刷新也失败
+
 ## 集成
 
-1. Spring-boot
+1. Spring Boot
 1. Quartz
-1. Spring-mail
+1. Spring Mail
 1. JdbcTemplate
+1. Spring Cloud Config
 
 ## TODO
 
-1. 集成spring cloud config
 1. logback根据spring profile更改level
 1. 增加导出文件类型export_file_type：csv、xls、xlsx
 1. 邮件发送的内容通过配置加载，并增加超级接收员
--- src/main/java/com/domain/butterfly/TimerApplication.java
+++ src/main/java/com/domain/butterfly/TimerApplication.java
@@ -2,6 +2,7 @@ package com.domain.butterfly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * com.domain.butterfly
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2017/11/10
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TimerApplication {
 
     public static void main(String[] args) {
